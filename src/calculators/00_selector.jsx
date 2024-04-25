81a58bb25e60003f06bb185d91e44a4c0617c4ae
Kazim Ozdil <skozdil@gmail.com>
2024-04-25 21:00:57 +0100

    JSXGraph added

--- src/calculators/00_selector.jsx
+++ src/calculators/00_selector.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import CalcRect from "./01_regtangular";
-import CalcTube from "./02_tube";
-import CalcCircular from "./03_circular";
-import CalcPipe from "./04_pipe";
-import CalcIBeam from "./05_ibeam";
-import CalcLBeam from "./06_lbeam";
-import CalcCBeam from "./07_cbeam";
-import CalcTBeam from "./08_tbeam";
+// import CalcTube from "./02_tube";
+// import CalcCircular from "./03_circular";
+// import CalcPipe from "./04_pipe";
+// import CalcIBeam from "./05_ibeam";
+// import CalcLBeam from "./06_lbeam";
+// import CalcCBeam from "./07_cbeam";
+// import CalcTBeam from "./08_tbeam";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -26,30 +26,30 @@ function Selector() {
     return ( 
     <>
         <div>
-            <select value={value} onChange={handleChange}>
+            <select value={value} onChange={handleChange} >
                 <option defaultValue="select-option">Select option</option>
                 <option value="CalcRect">Regtangular</option>
-                <option value="CalcTube">Tube</option>
+                {/* <option value="CalcTube">Tube</option>
                 <option value="CalcCircular">Circular</option>
                 <option value="CalcPipe">Pipe</option>
                 <option value="CalcIBeam">I-Beam</option>
                 <option value="CalcLBeam">L-Beam</option>
                 <option value="CalcCBeam">C-Beam</option>
-                <option value="CalcTBeam">T-Beam</option>
+                <option value="CalcTBeam">T-Beam</option> */}
             </select>
 
             {(() => {
                 switch(value) {
                     case "select-option" : return "";
                     case "CalcRect" : return <CalcRect h0={150} w0={100}/>;
-                    case "CalcTube" : return <CalcTube h0={150} w0={100} t0={3}/>;
-                    case "CalcCircular" : return <CalcCircular d0={100} />;
-                    case "CalcPipe" : return <CalcPipe d0={100} t0={5} />;
-                    case "CalcIBeam" : return <CalcIBeam h0={300} tw0={25} wfb0={350} tfb0={15} wft0={250} tft0={20}/>;
-                    case "CalcLBeam" : return <CalcLBeam h0={150} tw0={20} wfb0={200} tfb0={25}/>;
-                    case "CalcCBeam" : return <CalcCBeam h0={300} tw0={50} wfb0={350} tfb0={50} wft0={250} tft0={50}/>;
-                    case "CalcTBeam" : return <CalcTBeam h0={150} tw0={50} wft0={150} tft0={50}/>;
-                    default :  return <CalcRect h0={150} w0={100}/>;
+                    // case "CalcTube" : return <CalcTube h0={150} w0={100} t0={3}/>;
+                    // case "CalcCircular" : return <CalcCircular d0={100} />;
+                    // case "CalcPipe" : return <CalcPipe d0={100} t0={5} />;
+                    // case "CalcIBeam" : return <CalcIBeam h0={300} tw0={25} wfb0={350} tfb0={15} wft0={250} tft0={20}/>;
+                    // case "CalcLBeam" : return <CalcLBeam h0={150} tw0={20} wfb0={200} tfb0={25}/>;
+                    // case "CalcCBeam" : return <CalcCBeam h0={300} tw0={50} wfb0={350} tfb0={50} wft0={250} tft0={50}/>;
+                    // case "CalcTBeam" : return <CalcTBeam h0={150} tw0={50} wft0={150} tft0={50}/>;
+                    // default :  return <CalcRect h0={150} w0={100}/>;
                 }
             })()}
 
--- src/calculators/01_regtangular.jsx
+++ src/calculators/01_regtangular.jsx
@@ -1,5 +1,5 @@
-import JXGBoard2 from 'jsxgraph-react-js'
-
+import { JSXGraph } from "jsxgraph";
+import JSXBoard from "./90_graph";
 function CalcRect(props) {
     let area, momentofInartiaX, momentofInartiaY, ix, iy, welxt, welyt, welxb, welyb;
     let h = props.h0;
@@ -16,25 +16,28 @@ function CalcRect(props) {
     welxb = welxt = momentofInartiaX / (w/2);
     welyb = welyt = momentofInartiaY / (h/2); 
 
-    let logicJS = (brd) => {
-        var A = brd.create('point', [0,0],{name:"", fixed:true,size: 0 }),
-        B = brd.create('point',  [w,0],{name:"", fixed:true,size: 0 }),
-        C = brd.create('point', [w,h],{name:"", fixed:true,size: 0 }),
-        D = brd.create('point',  [0,h],{name:"", fixed:true,size: 0 }),
-        Z = brd.create('point',  [0,0],{name:"0", fixed:true,size: 5}),
-        rect = brd.create('polygon',[A,B,C,D],{hasInnerPoints:false, strokeWidth: 0, fillColor: "blue", fillOpacity: 1});
-    }
-
+    // let logicJS = (brd) => {
+    //     var A = brd.create('point', [0,0],{name:"", fixed:true,size: 0 }),
+    //     B = brd.create('point',  [w,0],{name:"", fixed:true,size: 0 }),
+    //     C = brd.create('point', [w,h],{name:"", fixed:true,size: 0 }),
+    //     D = brd.create('point',  [0,h],{name:"", fixed:true,size: 0 }),
+    //     Z = brd.create('point',  [0,0],{name:"0", fixed:true,size: 5}),
+    //     rect = brd.create('polygon',[A,B,C,D],{hasInnerPoints:false, strokeWidth: 0, fillColor: "blue", fillOpacity: 1});
+    // }
+    
+    
     return (  
         <>
-        <JXGBoard2
+        {/* <JXGBoard2
           logic={logicJS}
           boardAttributes={{ 
             axis: true, 
             boundingbox: [-100, +h+100, +w+100, -100],
             fixed: true,
         }}
-        />
+        /> */}
+
+        <JSXBoard />
         <p>Regtangular Calculation</p>
         <p>Area = {area}</p>
         <p>Moment of Inertia at X Axis = {momentofInartiaX}</p>
